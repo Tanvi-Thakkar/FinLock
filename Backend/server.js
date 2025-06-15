9792f4c7959d1e2b15e11ac402061a64b29e4b09
Tanvi Thakkar <thakkartanvi03@gmail.com>
2025-06-15 15:54:15 +0530

    changes in env file

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,7 @@ app.use(
     })
 );
 
-app.use(express.json());
+app.use(express.json());  //When a client sends data via POST, PUT, or PATCH as JSON (Content-Type: application/json), this middleware parses it and makes it available in req.body.
 
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`server running on port ${PORT}`)); //starts the server and listens to the incoming requests
