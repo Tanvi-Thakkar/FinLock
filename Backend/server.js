705334be859e8ba95c7b8dc16cf4f2511b1f987d
Tanvi Thakkar <thakkartanvi03@gmail.com>
2025-06-16 00:07:21 +0530

    mongoDB connected :)

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require('./config/db');
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -15,6 +17,10 @@ app.use(
 );
 
 app.use(express.json());  //When a client sends data via POST, PUT, or PATCH as JSON (Content-Type: application/json), this middleware parses it and makes it available in req.body.
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`server running on port ${PORT}`)); //starts the server and listens to the incoming requests
